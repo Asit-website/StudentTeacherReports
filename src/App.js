bc647f503fa338ddf622236b9cecaa82df535c3d
Asit-website <asitmandal492@gmail.com>
2022-02-19 17:21:51 +0530

    This is the report section of student and teacher dashboard

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
+import Student from './components/student/Student';
+import Teacher from './components/Teacher/Teacher';
+import Render from './components/Render';
+import StudentRouter from './components/student/StudentRouter';
+import TeacherRouter from './components/Teacher/TeacherRouter';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+     <Render/>
+     <Routes>
+       <Route exact path='/student' element={<Student/>}/>
+       <Route exact path='/teacher' element={<Teacher/>}/>
+       
+     </Routes>
+     {/* ======Student Route======= */}
+      <StudentRouter/>
+     {/* =====Teacher Router========  */}
+     <TeacherRouter/>
+
+    </Router>
   );
 }
 
